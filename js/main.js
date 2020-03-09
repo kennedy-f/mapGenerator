1aa07978756fbf770f004953f631db590c1eb631
KennedyOn <kennedy2000silva@hotmail.com>
2020-03-08 23:57:24 -0300

    Inicio matriz

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
-var matriz = ['arroz', 'feijao', 'batata', 'ovo']; 
+console.log('Digite generateMap'); 
+console.log('Está function que esta a preencher a matriz')
 
-console.log(matriz); 
+var types = ['florest', 'mountains', 'plains']; 
+
+function createArray2D(rows){
+    var arr = [];
+
+    for (var i = 0; i < rows; i++) {
+        arr[i] = [];
+    }
+
+    return arr;
+}
+
+var matriz
+function generateMap(size){
+    var matriz = createArray2D(size); 
+    for (var i = 0 ; i< size; i++){ 
+        for(var j = 0; j < size; j++ ){ 
+            matriz[i][j] = 'teste'; 
+        }
+    }
+    return matriz; 
+}
+
 
 //se quiser mostrar em tela só descomentar isso 
 $(document).ready(function(){
-    matriz.map(dado => $("<p>"+ dado +  "</p>").insertAfter($("#matriz"))); 
+    types.map(dado => $("<p>"+ dado +  "</p>").insertAfter($("#matriz"))); 
 })
 
