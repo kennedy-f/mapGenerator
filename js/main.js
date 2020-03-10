cfaf8b23f193d05cfb77a004b58852d562c7d0d6
KennedyOn <kennedy2000silva@hotmail.com>
2020-03-10 00:25:38 -0300

    Atualizaçao gian

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
     console.log('Digite generateMap');
     console.log('Está function que esta a preencher a matriz')
-    showTypes(); 
+    showTypes();
 })
 
 
 //Types 
-var types = ['florest', 'mountains', 'plains', 'cities', 'water']; 
+var types = ['forest', 'mountain', 'plain', 'citie', 'water', 'cave', 'desert', 'lava'];
 
 function showTypes() {
-    types.map(dado => $("<label>" + dado + ",  </label><span> </span>" ).appendTo($("#selectedTypes")));
+    types.map(dado => $("<label>" + dado + ",  </label><span> </span>").appendTo($("#selectedTypes")));
 }
 
 function deleteTypes() {
@@ -38,7 +38,7 @@ function addGroundType(event) {
 }
 
 //Map 
-function createArray2D(rows){
+function createArray2D(rows) {
     var arr = [];
     for (var i = 0; i < rows; i++) {
         arr[i] = [];
@@ -47,26 +47,45 @@ function createArray2D(rows){
 }
 
 
-function generateMap(size){
-    var matriz = createArray2D(size); 
-    for (var i = 0 ; i< size; i++){ 
-        for(var j = 0; j < size; j++ ){ 
-            matriz[i][j] = types[Math.floor(Math.random() * types.length)]; 
+function generateMap(size) {
+    var matriz = createArray2D(size);
+    for (var i = 0; i < size; i++) {
+        for (var j = 0; j < size; j++) {
+            matriz[i][j] = [] //types[Math.floor(Math.random() * types.length)];
         }
     }
-    return matriz; 
+    return matriz;
 }
 
-function showMap(mapX, mapY){ //to-do incrementar funcao de tamanho X e Y no mapa
-    var map = generateMap(mapX);         
-    for (var i = 0; i < mapX; i++) { 
-        for (var j = 0; j < mapX; j++ ){             
-            $('<label >' + map[i][j]  + '</lavel><span>, <span>').appendTo($('#map'));
-        }
-        $('</br>').appendTo($('#map')); 
+function generateContinent(matriz) {
+    //usando drunkenwalker
+    var y = Math.random(), x = Math.random();
+    matriz[y][x] = 1
+    for (var i = 0; i < 100; i++) {
+        var dir = Math.random() * 3
+        if (dir == 0)
+            y -= 1
+        else if (dir == 1)
+            x += 1
+        else if (dir == 2)
+            y += 1
+        else if (dir == 3)
+            x -= 1
+        matriz[y][x] = 1
     }
+    return matriz
+}
 
 
+
+function showMap(mapX, mapY) { //to-do incrementar funcao de tamanho X e Y no mapa
+    var map = generateMap(mapX);
+    for (var i = 0; i < mapX; i++) {
+        for (var j = 0; j < mapX; j++) {
+            $('<label >' + map[i][j] + '</lavel><span>, <span>').appendTo($('#map'));
+        }
+        $('</br>').appendTo($('#map'));
+    }
 }
 
 
