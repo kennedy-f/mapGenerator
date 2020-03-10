0165bd568125ef4f03bcff814c2dce7d92ef4c63
giangamberi <giangamberi@hotmail.com>
2020-03-10 19:29:47 -0300

    eae

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ function generateMap(size){
     return matriz; 
 }
 
-//kennedy corno
+//kennedy corno 2.0
 function generateContinent(matriz){
     //usando drunkenwalker
     var y=Math.random(),x = Math.random();
